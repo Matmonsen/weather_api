426b0a6d89480e5f4aeb95cafbf57c059ada08e2
Matmonsen <sindre.nerdal@gmail.com>
2017-01-08 21:27:07 +0100

    No more 500 error in search

--- api/views.py
+++ api/views.py
@@ -47,28 +47,40 @@ def search(request: HttpRequest) -> JsonResponse:
 
     # Fetches the data
     try:
-        forecast = Forecast.objects\
-            .filter(search=location, language=language, forecast_type=forecast_type)\
+        forecast = Forecast.objects \
+            .filter(search=location, language=language, forecast_type=forecast_type) \
             .latest('created')
         # Forecasts is cached and cache is NOT too old
         if time_is_less_then_x_minutes_ago(forecast.created, 10):
             time_list = Time.objects.filter(forecast=forecast.id)
+            format_response(response, time_list, forecast, language, forecast_type)
         else:
             # Forecasts is cached, but the cache IS too old
             yr = Yr(location, language, forecast_type)
             yr.download()
-            forecast, time_list = save_weather_data(yr)
+
+            if yr.source_data:
+                forecast, time_list = save_weather_data(yr)
+                format_response(response, time_list, forecast, language, forecast_type)
+            else:
+                response['success'] = False
+                response['message'] = 'An error occurred'
     except Forecast.DoesNotExist:
         yr = Yr(location, language, forecast_type)
         yr.download()
-        forecast, time_list = save_weather_data(yr)
+        if yr.source_data:
+            forecast, time_list = save_weather_data(yr)
+            format_response(response, time_list, forecast, language, forecast_type)
+        else:
+            response['success'] = False
+            response['message'] = 'An error occurred'
 
+    return JsonResponse(response)
+
+
+def format_response(response, time_list, forecast, language, forecast_type):
     # format response
     forecasts = [time_data.to_dict() for time_data in time_list]
-
     response['data']['meta'] = forecast.to_dict()
     response['data']['forecasts'] = cleanup_response(forecasts, language, forecast_type)
     response['data']['lastModified'] = '{0}Z'.format(datetime.datetime.now(), '01')
-    response['success'] = True
-
-    return JsonResponse(response)
